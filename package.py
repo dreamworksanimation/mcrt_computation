ee97990e77df2de82631c2c8c726f4777e7ec3c0
rwoods <ron.woods@dreamworks.com>
2023-04-05 10:53:22 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '11.6'
+    _version = '11.7'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -64,10 +64,10 @@ sconsTargets = {
 
 requires = [
     'arras4_core-4.10',
-    'mcrt_dataio-11.6',
-    'mcrt_messages-10.4',
-    'moonray-13.6',
-    'scene_rdl2-11.6',
+    'mcrt_dataio-11.7',
+    'mcrt_messages-10.5',
+    'moonray-13.7',
+    'scene_rdl2-11.7',
 ]
 
 private_build_requires = [
