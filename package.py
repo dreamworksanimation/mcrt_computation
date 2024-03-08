0fe06e14ddab68ca69867e305714613ab79f40fa
Ron Woods <ron.woods@dreamworks.com>
2024-03-08 07:54:26 -0800

    MOONRAY-5137, MOONRAY-5212: Remove scons build capability. (#151)

--- package.py
+++ package.py
@@ -2,10 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # -*- coding: utf-8 -*-
-import os, sys
-
-unittestflags = (['@run_all', '--unittest-xml']
-                 if os.environ.get('BROKEN_CUSTOM_ARGS_UNITTESTS') else [])
+import os
 
 name = 'mcrt_computation'
 
@@ -31,13 +28,6 @@ authors = [
 
 help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
-
-if 'scons' in sys.argv:
-    build_system = 'scons'
-    build_system_pbr = 'bart_scons-10'
-else:
-    build_system = 'cmake'
-    build_system_pbr = 'cmake_modules'
 
 variants = [
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1'],
@@ -65,14 +55,6 @@ variants = [
 conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
-scons_targets = ['@install'] + unittestflags
-sconsTargets = {
-    'refplat-vfx2020.3': scons_targets,
-    'refplat-vfx2021.0': scons_targets,
-    'refplat-vfx2022.0': scons_targets,
-    'refplat-vfx2023.0': scons_targets,
-}
-
 requires = [
     'arras4_core-4.10',
     'mcrt_dataio-13.13',
@@ -82,7 +64,7 @@ requires = [
 ]
 
 private_build_requires = [
-    build_system_pbr,
+    'cmake_modules',
     'cppunit',
 ]
 
@@ -93,15 +75,10 @@ testentry = lambda i: ("variant%d" % i,
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
-if build_system is 'cmake':
-    def commands():
-        prependenv('CMAKE_PREFIX_PATH', '{root}')
-        prependenv('LD_LIBRARY_PATH', '{root}/lib64:{root}/dso')
-        prependenv('PATH', '{root}/bin')
-else:
-    def commands():
-        prependenv('LD_LIBRARY_PATH', '{root}/lib:{root}/dso')
-        prependenv('PATH', '{root}/bin')
+def commands():
+    prependenv('CMAKE_PREFIX_PATH', '{root}')
+    prependenv('LD_LIBRARY_PATH', '{root}/lib64:{root}/dso')
+    prependenv('PATH', '{root}/bin')
 
 uuid = '9b2f7154-23bd-4b97-9aa9-1fcdaf6d2a05'
 
