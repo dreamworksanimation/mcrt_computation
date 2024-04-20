829dad77958febd3230b48900c8eb150bf9bc0b9
jlanz <Jon.Lanz@dreamworks.com>
2024-04-19 20:18:49 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.1'
+    _version = '14.2'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -52,10 +52,10 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
     'arras4_core-4.10',
-    'mcrt_dataio-14.0',
-    'mcrt_messages-13.0',
-    'moonray-16.1',
-    'scene_rdl2-14.0',
+    'mcrt_dataio-14.1',
+    'mcrt_messages-13.1',
+    'moonray-16.2',
+    'scene_rdl2-14.1',
 ]
 
 private_build_requires = [
