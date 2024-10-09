4a0de2da8e89452270018eef55e60f774766e53c
bart <build.and.release.tools@dreamworks.com>
2024-10-09 11:27:05 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '14.26'
+    _version = '14.27'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -54,10 +54,10 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
     'arras4_core-4.10',
-    'mcrt_dataio-14.15',
-    'mcrt_messages-13.3',
-    'moonray-16.26',
-    'scene_rdl2-14.15',
+    'mcrt_dataio-14.16',
+    'mcrt_messages-13.4',
+    'moonray-16.27',
+    'scene_rdl2-14.16',
 ]
 
 private_build_requires = [
