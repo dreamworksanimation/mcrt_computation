dd9b69e98326c99544737664a10ff11b6da55190
206531218_nbcuni <toshi.kato@dreamworks.com>
2025-02-25 15:04:47 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '15.3'
+    _version = '15.4'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -55,10 +55,10 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
     'arras4_core-4.10',
-    'mcrt_dataio-15.2',
+    'mcrt_dataio-15.3',
     'mcrt_messages-14.0',
-    'moonray-17.3',
-    'scene_rdl2-15.2',
+    'moonray-17.4',
+    'scene_rdl2-15.3',
 ]
 
 private_build_requires = [
